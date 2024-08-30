ff702ae2c19bca2ae94ea6cd13c2b8346fb28815
vamsibyramala <70326727+vamsibyramala@users.noreply.github.com>
2024-08-30 04:16:33 +0000

    removed access key

--- .devcontainer/ec2.tf
+++ .devcontainer/ec2.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA2DEAK7LOVHAQSIFE"
-  secret_key = "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"
 }
 resource "aws_instance" "test" {
   ami = "ami-02b49a24cfb95941c"
