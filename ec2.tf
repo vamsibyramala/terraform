87f73dd2993782e1e896d7c21801d46124ebc8c7
vamsibyramala <70326727+vamsibyramala@users.noreply.github.com>
2024-08-30 10:06:04 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,15 @@
 provider "aws" {
   region = "ap-south-1"
 }
+variable "ami" {
+  default = "ami-02b49a24cfb95941c"  
+}
+variable "type" {
+  default = "t2.micro"
+}
 resource "aws_instance" "new" {
-  ami = "ami-02b49a24cfb95941c"
-  instance_type = "t2.micro"
+  ami = var.ami
+  instance_type = var.type
   key_name = "new-mumbai"
   tags = {
     Name = "terraform"
